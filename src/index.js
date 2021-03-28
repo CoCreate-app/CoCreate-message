c58d72a50be4d3b2558a658a7254e179aaeb9656
frankpagan <frank@cocreate.app>
2021-03-28 17:05:53 +0000

    First Commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import {getCommonParams, getCommonParamsExtend, generateSocketClient} from "../../../CoCreateJS/src/common-fun.js"
+
+const CoCreateMessage = {
+  socket: null,
+  setSocket: function(socket) {
+    this.socket = socket;
+  },
+ /*
+ CoCreate.message.send({
+    namespace: '',
+    room: '',
+    broadcast: true/false,
+    broadcast_sender: true/false
+    
+    rooms: [r1, r2],
+    emit: {
+      message': 'nice game',
+      data': 'let's play a game ....'
+    }
+  })
+ */
+ send: function(data) {
+    let request_data = getCommonParams();
+    
+    if (!data || !data.emit) {
+      return;     
+    }
+    request_data = {...request_data, ...data}
+    
+    /** socket parameters **/
+    // if (data['broadcast'] === undefined) {
+    //   request_data['broadcast'] = true;
+    // }
+    // if (data['broadcast_sender'] === undefined) {
+    //   request_data['broadcast_sender'] = true;
+    // }
+    const room = generateSocketClient(data.namespace, data.room);
+    
+    this.socket.send('sendMessage', request_data, room)
+ },
+ 
+ receive: function(message, fun) {
+   this.socket.listen(message, fun);
+ },
+}
+
+export default function Message(socket) {
+  CoCreateMessage.setSocket(socket)
+  return CoCreateMessage;
+};
+
