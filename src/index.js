de548e1d45314fc4e12df082df0c5189f0445242
Frank <frank@cocreate.app>
2021-03-30 21:38:15 +0000

    fix: import paths using npm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import CoCreateSocket from "@cocreate/socket"
 import {getCommonParams, getCommonParamsExtend, generateSocketClient} from "@cocreate/crud/src/common-fun.js"
 
-let socket = new CoCreateSocket('ws');
+let message_socket = new CoCreateSocket('ws');
 
 const CoCreateMessage = {
   socket: null,
@@ -22,7 +22,7 @@ const CoCreateMessage = {
     }
   })
  */
- send: function(data) {
+  send: function(data) {
     let request_data = getCommonParams();
     
     if (!data || !data.emit) {
@@ -40,15 +40,42 @@ const CoCreateMessage = {
     const room = generateSocketClient(data.namespace, data.room);
     
     this.socket.send('sendMessage', request_data, room)
- },
+  },
  
- receive: function(message, fun) {
-   this.socket.listen(message, fun);
- },
+  receive: function(message, fun) {
+    this.socket.listen(message, fun);
+  },
+ 
+ 	createSocket: function(host, namespace) {
+ 	  if (!this.socket) return;
+ 	  
+		if (namespace) {
+			this.socket.create({
+				namespace: namespace, 
+				room: null,
+				host: host
+			});
+			this.socket.setGlobalScope(namespace);
+		} else {
+			this.socket.create({
+				namespace: null, 
+				room: null,
+				host: host
+			});
+		}
+	},
+	
+	listen: function(message, fun) {
+    this.socket.listen(message, fun);
+  },
+  
+  listenAsync: function(eventname) {
+    this.socket.listenAsync(eventname);
+  },
 }
 
-export default function Message(socket) {
-  CoCreateMessage.setSocket(socket)
-  return CoCreateMessage;
-};
+CoCreateMessage.setSocket(message_socket);
+CoCreateMessage.createSocket(window.config.host ? window.config.host : 'server.cocreate.app', window.config.organization_Id)
+
+export default CoCreateMessage;
 
